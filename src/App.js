ea5b326c00ddd4468b5eb55679319b512907f053
tgashwinkumar <tgashwinkumar@gmail.com>
2023-02-10 10:48:03 +0530

    Event responsive done !

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
-import Portal, { PortalAnother } from "./pages/Portal";
-import PortalNew from "./pages/PortalNew"
 import Register from "./pages/Register";
+import PortalWrapper from "./pages/PortalWrapper";
+import Event from "./pages/Event";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* <Route path="team" element={<OurTeam />} />
-        <Route path="event/:id" element={<Event />} /> */}
-        <Route path="portal" element={<Portal />} />/
+        <Route path="portal" element={<PortalWrapper />}>
+          <Route path="event" element={<Event />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
         <Route path="register" element={<Register />} />
-        <Route path="another-portal" element={<PortalAnother />} />
-        <Route path="portal-new" element={<PortalNew />} />
         <Route index element={<Landing />} />
       </Routes>
     </BrowserRouter>
