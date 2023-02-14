b3f18952aac6e1648e4d9ba7591791d25cbc2e2a
tgashwinkumar <tgashwinkumar@gmail.com>
2023-02-14 19:46:14 +0530

    Paper presentations bug fixed.

---
+++ src/pages/sections/Section1 copy.js
@@ -0,0 +1,312 @@
+import React, { useEffect } from "react";
+
+//  Landing
+
+const Section1 = () => {
+  return (
+    <section
+      id="section1"
+      className="h-screen w-screen relative overflow-hidden"
+    >
+      <div className=" hidden lg:flex w-full h-full justify-center items-center overflow-hidden bg[rgb(18,18,18)] relative z-10">
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-48 relative">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874405/bridge_glqanj.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            id="duotone-1"
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874411/DSC_0053_1_tlaozd.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            id="duotone-2"
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874404/IMG_4981_ew0rlv.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874653/1674282463303_jhrpv2.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-96">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874653/1674282463303_jhrpv2.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874773/DSC_0100-min_lew0tn.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-32">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675874771/1674282462996-min_d9ixck.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full aspect-[3/4]"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675875367/1674280844981_qosaib.jpg)",
+
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-36">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675875371/IMG_8320_exn4fp.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full aspect-[3/4]"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675875037/IMG_1302_beaqti.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-96">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675875364/IMG_2513_j71zpl.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background:
+                "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1675875371/DSC_0021_dcvv0j.jpg)",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+        <div className="w-1/6 h-full space-y-6 px-6 -mt-24">
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+          <div
+            className="w-full relative aspect-[3/4] rounded-lg shadow-2xl bg-black [box-shadow:inset_0_0_3rem_black] hover:scale-105 transition-all"
+            style={{
+              background: "url()",
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+            
+          ></div>
+        </div>
+      </div>
+
+      <div className="bg-[#121212] w-full h-full lg:hidden">
+        <div
+          className="w-full h-full lg:w-64 lg:h-64"
+          style={{
+            background:
+              "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1676036256/KriyaFinal_lj4u2j.png)",
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+        {/* <div className="w-full h-full absolute z-50"
+          style={{
+            background: "url(https://i.ibb.co/QHfydfF/Kriya-Logo-2023-white-1.png)",
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div> */}
+      </div>
+      <div className="bg-[#121212] w-full h-full absolute top-0 hidden lg:block">
+        <div className="w-full h-full" style={{
+          background: "url(https://res.cloudinary.com/dksmk66vo/image/upload/v1676036256/KriyaFinal_lj4u2j.png)",
+            backgroundPosition: "center",
+            backgroundSize: "contain",
+            backgroundRepeat: "no-repeat",
+          }}
+        ></div>
+      </div>
+    </section>
+  );
+};
+
+export default Section1;
