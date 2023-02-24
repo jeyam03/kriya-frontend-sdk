4150477199b972c69f852a59259f61882eee27ab
tgashwinkumar <tgashwinkumar@gmail.com>
2023-02-25 01:39:59 +0530

    Payment redirects successful.

--- src/pages/MiddlePorts/PaymentSuccess.js
+++ src/pages/MiddlePorts/PaymentSuccess.js
@@ -5,6 +5,7 @@ import {
   fetchUserVerifyConfirm,
 } from "../../API/call";
 import { useNavigate, useParams, useSearchParams } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const PSG_COLLEGE =
   "PSG College of Technology (Autonomous), Peelamedu, Coimbatore District 641004";
@@ -20,11 +21,42 @@ const PaymentSuccess = () => {
       .then((res) => {
         if (res.data.data.status !== "SUCCESS")
           navigate(`/payment/failure?txn=${searchParams.get("txn")}`);
-        if (searchParams.get("redirect") === "complete_register")
-          navigate(
-            `/auth/?type=signup&page=final&email=${res.data.data.email}`
-          );
-        else navigate(`/`);
+        console.log(
+          localStorage.getItem("txn_redirect_uri"),
+          searchParams.get("txn"),
+          localStorage.getItem("txn")
+        );
+        if (searchParams.get("redirect") === "complete_register") {
+          if (
+            localStorage.getItem("txn_redirect_uri") &&
+            searchParams.get("txn") === localStorage.getItem("txn")
+          ) {
+            localStorage.removeItem("txn");
+            const navigateUrl = localStorage.getItem("txn_redirect_uri");
+            localStorage.removeItem("txn_redirect_uri");
+            navigate(navigateUrl);
+          } else {
+            navigate(
+              `/auth/?type=signup&page=final&email=${res.data.data.email}`
+            );
+          }
+        } else if (searchParams.get("redirect") === "workshop_register") {
+          if (
+            localStorage.getItem("txn_redirect_uri") &&
+            searchParams.get("txn") === localStorage.getItem("txn")
+          ) {
+            localStorage.removeItem("txn");
+            const navigateUrl = localStorage.getItem("txn_redirect_uri");
+            localStorage.removeItem("txn_redirect_uri");
+            navigate(navigateUrl);
+          } else {
+            toast.error("Payment Fetch failed. Verify payment here.");
+            navigate("/portal/profile");
+          }
+        } else {
+          toast.error("Payment Fetch failed. Verify payment here.");
+          navigate("/portal/profile");
+        }
       })
       .catch((err) => console.log(err));
   }, [searchParams]);
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { AiOutlineUser } from "react-icons/ai";
 import { GrTransaction, GrWorkshop } from "react-icons/gr";
-import { MdEventAvailable, MdOutlineCancel, MdOutlineEmojiEvents } from "react-icons/md";
+import {
+  MdEventAvailable,
+  MdOutlineCancel,
+  MdOutlineEmojiEvents,
+} from "react-icons/md";
 import { BsCheck2Circle } from "react-icons/bs";
-import { fetchEventDetailsByEmail, fetchEvents, fetchPaymentDetailsByEmail, fetchUserByEmail, fetchWorkshops } from "../API/call";
+import {
+  fetchEventDetailsByEmail,
+  fetchEvents,
+  fetchPaymentDetailsByEmail,
+  fetchUserByEmail,
+  fetchWorkshops,
+} from "../API/call";
 import { IoIosArrowForward } from "react-icons/io";
 import { Link } from "react-router-dom";
-
 
 const Profile = () => {
   const [userDetails, setUserDetails] = useState(null);
@@ -23,7 +32,12 @@ const Profile = () => {
   useEffect(() => {
     fetchPaymentDetailsByEmail(localStorage.getItem("email")).then((res) => {
       console.log(res.data.data);
-      setPaymentDetails(res.data.data);
+      setPaymentDetails(
+        res.data.data
+          .filter((i) => i.status !== "INITIATED")
+          .sort((a, b) => a.datetime.localeCompare(b.datetime))
+          .reverse()
+      );
     });
   }, []);
 
@@ -50,13 +64,14 @@ const Profile = () => {
     <section className="w-screen font-poppins h-screen overflow-x-hidden overflow-y-scroll py-24 lg:pt-0">
       <div className="hidden lg:block w-full h-36 bg-gradient-to-r from-[#C80067] to-[#5451B6]"></div>
       <div className="lg:px-16 py-12 text-white flex flex-col items-center lg:items-start">
-        <div className="h-48 w-48 rounded-full lg:-mt-36"
-        style={{
-          backgroundImage: `url(${userDetails?.profilePhoto})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
+        <div
+          className="h-48 w-48 rounded-full lg:-mt-36"
+          style={{
+            backgroundImage: `url(${userDetails?.profilePhoto})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+          }}
         ></div>
         <h1 className="text-4xl mt-8 font-bold text-white text-center lg:text-left">
           {userDetails ? userDetails?.name : "XXXXX"}
@@ -73,10 +88,26 @@ const Profile = () => {
               <h1 className="text-2xl">About</h1>
             </div>
             <div className="grid grid-cols-1 gap-y-1   w-full mt-8 space-y-1  ">
-              <TextOutput className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid" heading="Name" content={userDetails.name} />
-              <TextOutput className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid" heading="Email" content={userDetails.email} />
-              <TextOutput className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid" heading="Phone" content={userDetails.phone} />
-              <TextOutput className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid" heading="College" content={userDetails.college} />
+              <TextOutput
+                className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid"
+                heading="Name"
+                content={userDetails.name}
+              />
+              <TextOutput
+                className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid"
+                heading="Email"
+                content={userDetails.email}
+              />
+              <TextOutput
+                className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid"
+                heading="Phone"
+                content={userDetails.phone}
+              />
+              <TextOutput
+                className="w-full lg:grid-cols-[100px_minmax(400px,1fr)] grid"
+                heading="College"
+                content={userDetails.college}
+              />
             </div>
           </div>
           <div className="w-full">
@@ -84,21 +115,24 @@ const Profile = () => {
               <GrTransaction className="text-2xl text-white invert" />
               <h1 className="text-2xl">Transactions</h1>
             </div>
-            <div className="mt-8 space-y-4">
-              {paymentDetails?.length === 0 && <div className="space-y-4">
-                <p className="text-lg">Uh oh! You have'nt made any transactions yet !</p>
-                <Link
-                  className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
-                  to="/../?sn=section5"
-                >
-                  <p className="">Pay general registration fee !</p>
-                  <IoIosArrowForward
-                    className="ml-1 group-hover:ml-2 transition-all"
-                    size={16}
-                  />
-                </Link>
-              </div>
-              }
+            <div className="mt-8 space-y-4 max-h-[40vh] overflow-y-auto pr-4">
+              {paymentDetails?.length === 0 && (
+                <div className="space-y-4">
+                  <p className="text-lg">
+                    Uh oh! You have'nt made any transactions yet !
+                  </p>
+                  <Link
+                    className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
+                    to="/../?sn=section5"
+                  >
+                    <p className="">Pay general registration fee !</p>
+                    <IoIosArrowForward
+                      className="ml-1 group-hover:ml-2 transition-all"
+                      size={16}
+                    />
+                  </Link>
+                </div>
+              )}
               {paymentDetails?.map((payment) => (
                 <div className="flex flex-row items-center space-x-4">
                   {payment.status === "SUCCESS" ? (
@@ -108,14 +142,38 @@ const Profile = () => {
                   )}
                   <div className="w-full">
                     <div className="flex items-center justify-between text-xs">
-                      <p className="">Transaction ID: {payment.transactionId}</p>
+                      <p className="">
+                        Transaction ID: {payment.transactionId}
+                      </p>
                       <div className="flex flex-col items-end">
-                        <p className="">{new Date(payment.datetime).toDateString()}</p>
-                        <p className="">{new Date(payment.datetime).toTimeString().split("GMT")[0]}</p>
+                        <p className="text-right">
+                          {new Date(payment.datetime).toDateString()}
+                        </p>
+                        <p className="text-right">
+                          {
+                            new Date(payment.datetime)
+                              .toTimeString()
+                              .split("GMT")[0]
+                          }
+                        </p>
                       </div>
                     </div>
-                    <div className={`${payment.status === "SUCCESS" ? "text-green-500" : "text-red-500"} flex items-center justify-between`}>
-                      <p className="text-lg w-5/6">{payment.eventId === "-1" ? "General" : "Workshop " + payment.eventId} registration {payment.status === "SUCCESS" ? "paid successfully" : "payment unsuccessful"}</p>
+                    <div
+                      className={`${
+                        payment.status === "SUCCESS"
+                          ? "text-green-500"
+                          : "text-red-500"
+                      } flex items-center justify-between`}
+                    >
+                      <p className="text-lg w-5/6">
+                        {payment.eventId === "-1"
+                          ? "General"
+                          : "Workshop " + payment.eventId}{" "}
+                        registration{" "}
+                        {payment.status === "SUCCESS"
+                          ? "paid successfully"
+                          : "payment unsuccessful"}
+                      </p>
                       <p className="text-lg">Rs. {payment.fee}</p>
                     </div>
                   </div>
@@ -130,20 +188,23 @@ const Profile = () => {
               <h1 className="text-2xl">Registered Events</h1>
             </div>
             <div className="mt-8 space-y-4">
-              {eventDetails?.length === 0 && <div className="space-y-4">
-                <p className="text-lg">Uh oh! You have'nt registered for any events yet !</p>
-                <Link
-                  className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
-                  to="/portal/event"
-                >
-                  <p className="">Register for events here !</p>
-                  <IoIosArrowForward
-                    className="ml-1 group-hover:ml-2 transition-all"
-                    size={16}
-                  />
-                </Link>
-              </div>
-              }
+              {eventDetails?.length === 0 && (
+                <div className="space-y-4">
+                  <p className="text-lg">
+                    Uh oh! You have'nt registered for any events yet !
+                  </p>
+                  <Link
+                    className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
+                    to="/portal/event"
+                  >
+                    <p className="">Register for events here !</p>
+                    <IoIosArrowForward
+                      className="ml-1 group-hover:ml-2 transition-all"
+                      size={16}
+                    />
+                  </Link>
+                </div>
+              )}
               {eventDetails?.map((event) => (
                 <div className="">
                   <div className="flex items-center justify-between text-xs">
@@ -153,10 +214,13 @@ const Profile = () => {
                   <div className="flex items-center justify-between">
                     <Link
                       to={`/portal/event/${event.eventId}`}
-                      className="text-lg hover:text-blue-400 hover:underline">
+                      className="text-lg hover:text-blue-400 hover:underline"
+                    >
                       {events.find((i) => i.id === event.eventId).name}
                     </Link>
-                    <p className="text-lg">{events.find((i) => i.id === event.eventId).time}</p>
+                    <p className="text-lg">
+                      {events.find((i) => i.id === event.eventId).time}
+                    </p>
                   </div>
                 </div>
               ))}
@@ -169,36 +233,47 @@ const Profile = () => {
               <h1 className="text-2xl">Registered Workshops</h1>
             </div>
             <div className="mt-8 space-y-4">
-              {paymentDetails?.filter((w) => w.type === "WORKSHOP").length === 0 && <div className="space-y-4">
-                <p className="text-lg">Uh oh! You have'nt registered for any workshops yet !</p>
-                <Link
-                  className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
-                  to="/../?sn=section5"
-                >
-                  <p className="">Register for workshops here !</p>
-                  <IoIosArrowForward
-                    className="ml-1 group-hover:ml-2 transition-all"
-                    size={16}
-                  />
-                </Link>
-              </div>
-              }
-              {paymentDetails?.filter((w) => w.type === "WORKSHOP").map((workshop) => (
-                <div className="">
-                  <div className="flex items-center justify-between text-xs">
-                    <p className="">Workshop ID: {workshop.eventId}</p>
-                    <p className="">Mar 24</p>
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <Link
-                      to={`/portal/workshop/${workshop.eventId}`}
-                      className="text-lg hover:text-blue-400 hover:underline">
-                      {fetchWorkshops().find((i) => i.wid === workshop.eventId).workName}
-                    </Link>
-                    <p className="text-lg">Rs. 250</p>
-                  </div>
+              {paymentDetails?.filter((w) => w.type === "WORKSHOP").length ===
+                0 && (
+                <div className="space-y-4">
+                  <p className="text-lg">
+                    Uh oh! You have'nt registered for any workshops yet !
+                  </p>
+                  <Link
+                    className="bg-blue-500 text-white w-fit px-4 py-2 rounded-xl text-sm flex items-center group"
+                    to="/../?sn=section5"
+                  >
+                    <p className="">Register for workshops here !</p>
+                    <IoIosArrowForward
+                      className="ml-1 group-hover:ml-2 transition-all"
+                      size={16}
+                    />
+                  </Link>
                 </div>
-              ))}
+              )}
+              {paymentDetails
+                ?.filter((w) => w.type === "WORKSHOP")
+                .map((workshop) => (
+                  <div className="">
+                    <div className="flex items-center justify-between text-xs">
+                      <p className="">Workshop ID: {workshop.eventId}</p>
+                      <p className="">Mar 24</p>
+                    </div>
+                    <div className="flex items-center justify-between">
+                      <Link
+                        to={`/portal/workshop/${workshop.eventId}`}
+                        className="text-lg hover:text-blue-400 hover:underline"
+                      >
+                        {
+                          fetchWorkshops().find(
+                            (i) => i.wid === workshop.eventId
+                          ).workName
+                        }
+                      </Link>
+                      <p className="text-lg">Rs. 250</p>
+                    </div>
+                  </div>
+                ))}
             </div>
           </div>
         </div>
