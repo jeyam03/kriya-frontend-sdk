c8832004ff95896ec82f143ae7f7892939c850e6
jeyam03 <64417749+jeyam03@users.noreply.github.com>
2023-03-21 18:08:35 +0530

    Acc Changes

--- src/pages/Enquiry/Accomodation.js
+++ src/pages/Enquiry/Accomodation.js
@@ -80,18 +80,10 @@ const Accomodation = () => {
   }, []);
 
   const handleProceed = () => {
-    setFormData({
-      ...formData,
-      days: dates.indexOf(formData.to) - dates.indexOf(formData.from),
-      amount: ((dates.indexOf(formData.to) - dates.indexOf(formData.from)) * roomCost[formData.roomType]) +
-        (50 * (formData.breakfast1 + formData.breakfast2 + formData.breakfast3 + formData.dinner1 + formData.dinner2 + formData.dinner3)) +
-        (formData.amenities === "Yes" && 100 * (dates.indexOf(formData.to) - dates.indexOf(formData.from)))
-    });
-
     toast.promise(fetchAccomodationRegister(formData), {
       loading: "Registering...",
       success: (res) => {
-        navigate("/acc-registered");
+        navigate("/portal/acc-registered");
         return "Registered Successfully";
       },
       error: (err) => {
@@ -444,6 +436,14 @@ const Accomodation = () => {
                             if (formData.city === "" || formData.residentialAddress === "" || formData.phone === "") {
                               toast.error("Please fill all the details");
                             } else {
+                              setFormData({
+                                ...formData,
+                                days: dates.indexOf(formData.to) - dates.indexOf(formData.from),
+                                amount: ((dates.indexOf(formData.to) - dates.indexOf(formData.from)) * roomCost[formData.roomType]) +
+                                  (50 * (formData.breakfast1 + formData.breakfast2 + formData.breakfast3 + formData.dinner1 + formData.dinner2 + formData.dinner3)) +
+                                  (formData.amenities === "Yes" && 100 * (dates.indexOf(formData.to) - dates.indexOf(formData.from)))
+                              });
+
                               handleProceed();
                             }
                           }}
